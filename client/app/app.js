065f0eb3cae8ddefdbe57b43dcff4b9cdba0a167
Alejandro <alexsalidoa@gmail.com>
2015-11-30 17:31:20 -0800

    Worked on chat toolbar and fixed contact list

--- client/app/app.js
+++ client/app/app.js
@@ -9,7 +9,7 @@ angular.module('chatApp', [
   'ngMaterial',
   'ngMessages'
 ])
-  .config(function ($routeProvider, $locationProvider, $httpProvider) {
+  .config(function ($routeProvider, $locationProvider, $httpProvider, $mdIconProvider) {
     $routeProvider
       .otherwise({
         redirectTo: '/'
@@ -17,6 +17,25 @@ angular.module('chatApp', [
 
     $locationProvider.html5Mode(true);
     $httpProvider.interceptors.push('authInterceptor');
+
+	//angular material icons
+	$mdIconProvider
+		.icon('plus_one', 'assets/svg/plus_one.svg')
+		.icon('attachment', 'assets/svg/attachment.svg')
+		.icon('chat_bubble', 'assets/svg/chat_bubble.svg')
+		.icon('close', 'assets/svg/close.svg')
+		.icon('contacts', 'assets/svg/contacts.svg')
+		.icon('face', 'assets/svg/face.svg')
+		.icon('facebook', 'assets/svg/facebook.svg')
+		.icon('fingerprint', 'assets/svg/fingerprint.svg')
+		.icon('info', 'assets/svg/info.svg')
+		.icon('more_vert', 'assets/svg/more_vert.svg')
+		.icon('search', 'assets/svg/search.svg')
+		.icon('search_black', 'assets/svg/search_black.svg')
+		.icon('twitter', 'assets/svg/twitter.svg')
+		.icon('insert_emoticon', 'assets/svg/insert_emoticon.svg')
+		.icon('send', 'assets/svg/send.svg')
+
   })
 
   .factory('authInterceptor', function ($rootScope, $q, $cookieStore, $location) {
--- client/app/dashboard/dashboard.controller.js
+++ client/app/dashboard/dashboard.controller.js
@@ -2,45 +2,50 @@
 
 angular.module('chatApp')
 	.controller('DashboardCtrl', function ($scope, $location, Auth) {
-		$scope.message = 'Hello';
 
 		$scope.logout = function () {
 			Auth.logout();
 			$location.path('/');
 		};
 
+		$scope.openChat = function (_id, ev) {
+			for (var i = 0; i < $scope.chats.length; i++) {
+				console.log($scope.chats[i]);
+				if ($scope.chats[i]._id === _id) {
+					$scope.chats[i].newMessage = false;
+					$scope.activeChat = $scope.chats[i];
+					$scope.apply();
+				}
+			}
+		};
+
 		//Fake chats
 		$scope.chats = [{
+			_id: 1,
 			name: 'Janet Perkins',
-			img: '/assets/svg/face.svg',
-			newMessage: true
+			img: undefined,
+			newMessage: false,
+			active: false,
 		}, {
+			_id: 2,
 			name: 'Mary Johnson',
-			img: '/assets/svg/face.svg',
-			newMessage: false
+			img: undefined,
+			newMessage: true,
+			active: false,
 		}, {
+			_id: 3,
 			name: 'Peter Carlsson',
-			img: '/assets/svg/face.svg',
-			newMessage: false
-		}, {
-			name: 'Janet Perkins',
-			img: '/assets/svg/face.svg',
-			newMessage: true
-		}, {
-			name: 'Mary Johnson',
-			img: '/assets/svg/face.svg',
-			newMessage: false
-		}, {
-			name: 'Peter Carlsson',
-			img: '/assets/svg/face.svg',
-			newMessage: false
-		}, {
-			name: 'Janet Perkins',
-			img: '/assets/svg/face.svg',
-			newMessage: true
-		}, {
-			name: 'Mary Johnson',
-			img: '/assets/svg/face.svg',
-			newMessage: false
+			img: '/assets/images/kitty.jpeg',
+			newMessage: false,
+			active: false,
 		}];
+
+		$scope.activeChat = $scope.chats[0];
+
+		$scope.$watch('activeChat', function (newVal, oldVal) {
+			oldVal.active = false;
+			newVal.active = true;
+			console.log('here');
+		})
+
 	});
