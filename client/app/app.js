003195b0a28c6027b812272de2c12ee65b149da4
Alejandro <alexsalidoa@gmail.com>
2016-08-22 19:29:06 -0700

    moved svg dir inside images and fixed dependency issues

--- client/app/app.js
+++ client/app/app.js
@@ -1,98 +1,98 @@
 'use strict';
 
 angular.module('chatApp', [
-		'ngCookies',
-		'ngResource',
-		'ngSanitize',
-		'ngRoute',
-		'btford.socket-io',
-		'ngMaterial',
-		'ngMessages',
-		'ngAnimate',
-		'vTabs',
-		'angularFileUpload',
-		'ngjsColorPicker'
-	])
-	.config(function ($routeProvider, $locationProvider, $httpProvider, $mdIconProvider, emojifyProvider) {
-		$routeProvider
-			.otherwise({
-				redirectTo: '/'
-			});
+        'ngCookies',
+        'ngResource',
+        'ngSanitize',
+        'ngRoute',
+        'btford.socket-io',
+        'ngMaterial',
+        'ngMessages',
+        'ngAnimate',
+        'vTabs',
+        'angularFileUpload',
+        'ngjsColorPicker'
+    ])
+    .config(function($routeProvider, $locationProvider, $httpProvider, $mdIconProvider, emojifyProvider) {
+        $routeProvider
+            .otherwise({
+                redirectTo: '/'
+            });
 
-		$locationProvider.html5Mode(true);
-		$httpProvider.interceptors.push('authInterceptor');
+        $locationProvider.html5Mode(true);
+        $httpProvider.interceptors.push('authInterceptor');
 
-		//angular material icons
-		$mdIconProvider
-			.icon('plus_one', 'assets/svg/plus_one.svg')
-			.icon('attachment', 'assets/svg/attachment.svg')
-			.icon('chat_bubble', 'assets/svg/chat_bubble.svg')
-			.icon('close', 'assets/svg/close.svg')
-			.icon('contacts', 'assets/svg/contacts.svg')
-			.icon('face', 'assets/svg/face.svg')
-			.icon('facebook', 'assets/svg/facebook.svg')
-			.icon('fingerprint', 'assets/svg/fingerprint.svg')
-			.icon('info', 'assets/svg/info.svg')
-			.icon('more_vert', 'assets/svg/more_vert.svg')
-			.icon('more_horiz', 'assets/svg/more_horiz.svg')
-			.icon('search', 'assets/svg/search.svg')
-			.icon('search_black', 'assets/svg/search_black.svg')
-			.icon('twitter', 'assets/svg/twitter.svg')
-			.icon('insert_emoticon', 'assets/svg/insert_emoticon.svg')
-			.icon('send', 'assets/svg/send.svg')
-			.icon('back', 'assets/svg/back.svg')
-			.icon('edit', 'assets/svg/edit.svg')
-			.icon('done', 'assets/svg/done.svg')
-			.icon('add', 'assets/svg/add.svg')
-			.icon('person_add', 'assets/svg/person_add.svg')
-			.icon('camera', 'assets/svg/camera.svg')
-			.icon('scribble', 'assets/svg/scribble.svg')
-			.icon('schedule', 'assets/svg/schedule.svg')
-			.icon('admin', 'assets/svg/admin.svg')
-			.icon('menu', 'assets/svg/menu.svg')
-			.icon('emojis-people', 'assets/svg/emojis-people.svg')
-			.icon('emojis-nature', 'assets/svg/emojis-nature.svg')
-			.icon('emojis-objects', 'assets/svg/emojis-objects.svg')
-			.icon('emojis-places', 'assets/svg/emojis-places.svg')
-			.icon('emojis-symbols', 'assets/svg/emojis-symbols.svg');
+        //angular material icons
+        $mdIconProvider
+            .icon('plus_one', 'assets/images/svg/plus_one.svg')
+            .icon('attachment', 'assets/images/svg/attachment.svg')
+            .icon('chat_bubble', 'assets/images/svg/chat_bubble.svg')
+            .icon('close', 'assets/images/svg/close.svg')
+            .icon('contacts', 'assets/images/svg/contacts.svg')
+            .icon('face', 'assets/images/svg/face.svg')
+            .icon('facebook', 'assets/images/svg/facebook.svg')
+            .icon('fingerprint', 'assets/images/svg/fingerprint.svg')
+            .icon('info', 'assets/images/svg/info.svg')
+            .icon('more_vert', 'assets/images/svg/more_vert.svg')
+            .icon('more_horiz', 'assets/images/svg/more_horiz.svg')
+            .icon('search', 'assets/images/svg/search.svg')
+            .icon('search_black', 'assets/images/svg/search_black.svg')
+            .icon('twitter', 'assets/images/svg/twitter.svg')
+            .icon('insert_emoticon', 'assets/images/svg/insert_emoticon.svg')
+            .icon('send', 'assets/images/svg/send.svg')
+            .icon('back', 'assets/images/svg/back.svg')
+            .icon('edit', 'assets/images/svg/edit.svg')
+            .icon('done', 'assets/images/svg/done.svg')
+            .icon('add', 'assets/images/svg/add.svg')
+            .icon('person_add', 'assets/images/svg/person_add.svg')
+            .icon('camera', 'assets/images/svg/camera.svg')
+            .icon('scribble', 'assets/images/svg/scribble.svg')
+            .icon('schedule', 'assets/images/svg/schedule.svg')
+            .icon('admin', 'assets/images/svg/admin.svg')
+            .icon('menu', 'assets/images/svg/menu.svg')
+            .icon('emojis-people', 'assets/images/svg/emojis-people.svg')
+            .icon('emojis-nature', 'assets/images/svg/emojis-nature.svg')
+            .icon('emojis-objects', 'assets/images/svg/emojis-objects.svg')
+            .icon('emojis-places', 'assets/images/svg/emojis-places.svg')
+            .icon('emojis-symbols', 'assets/images/svg/emojis-symbols.svg');
 
-		emojifyProvider.setConfig('assets/emojis');
+        // emojifyProvider.setConfig('assets/emojis');
 
-	})
+    })
 
-.factory('authInterceptor', function ($rootScope, $q, $cookieStore, $location) {
-	return {
-		// Add authorization token to headers
-		request: function (config) {
-			config.headers = config.headers || {};
-			if ($cookieStore.get('token')) {
-				config.headers.Authorization = 'Bearer ' + $cookieStore.get('token');
-			}
-			return config;
-		},
+.factory('authInterceptor', function($rootScope, $q, $cookieStore, $location) {
+    return {
+        // Add authorization token to headers
+        request: function(config) {
+            config.headers = config.headers || {};
+            if ($cookieStore.get('token')) {
+                config.headers.Authorization = 'Bearer ' + $cookieStore.get('token');
+            }
+            return config;
+        },
 
-		// Intercept 401s and redirect you to home
-		responseError: function (response) {
-			if (response.status === 401) {
-				$location.path('/');
-				// remove any stale tokens
-				$cookieStore.remove('token');
-				return $q.reject(response);
-			} else {
-				return $q.reject(response);
-			}
-		}
-	};
+        // Intercept 401s and redirect you to home
+        responseError: function(response) {
+            if (response.status === 401) {
+                $location.path('/');
+                // remove any stale tokens
+                $cookieStore.remove('token');
+                return $q.reject(response);
+            } else {
+                return $q.reject(response);
+            }
+        }
+    };
 })
 
-.run(function ($rootScope, $location, Auth) {
-	// Redirect to home if route requires auth and you're not logged in
-	$rootScope.$on('$routeChangeStart', function (event, next) {
-		Auth.isLoggedInAsync(function (loggedIn) {
-			if (next.authenticate && !loggedIn) {
-				event.preventDefault();
-				$location.path('/');
-			}
-		});
-	});
+.run(function($rootScope, $location, Auth) {
+    // Redirect to home if route requires auth and you're not logged in
+    $rootScope.$on('$routeChangeStart', function(event, next) {
+        Auth.isLoggedInAsync(function(loggedIn) {
+            if (next.authenticate && !loggedIn) {
+                event.preventDefault();
+                $location.path('/');
+            }
+        });
+    });
 });
--- client/app/dashboard/services/emojify/emojify.service.js
+++ client/app/dashboard/services/emojify/emojify.service.js
@@ -1,23 +1,24 @@
 'use strict';
 
 angular.module('chatApp')
-	.provider('emojify', function () {
-		// AngularJS will instantiate a singleton by calling "new" on this function
-		emojify.emojis = {};
-		emojify.DOMLoaded = {};
+    .provider('emojify', function() {
+        // AngularJS will instantiate a singleton by calling "new" on this function
+        var emojify = {};
+        emojify.emojis = {};
+        emojify.DOMLoaded = {};
 
-		return {
-			setEmojis: function (_emojis) {
-				emojify.emojis = _emojis;
-			},
+        return {
+            setEmojis: function(_emojis) {
+                emojify.emojis = _emojis;
+            },
 
-			setConfig: function (dir) {
-				emojify.setConfig({
-					img_dir: dir
-				});
-			},
-			$get: function () {
-				return emojify;
-			}
-		};
-	});
+            setConfig: function(dir) {
+                emojify.setConfig({
+                    img_dir: dir
+                });
+            },
+            $get: function() {
+                return emojify;
+            }
+        };
+    });
